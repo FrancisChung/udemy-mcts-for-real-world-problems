0183644e6e2d379fd556eca272074f4066009125
Francis Chung <francis.c.chung@gmail.com>
2025-07-29 18:37:19 +1000

    partial version

--- monte_carlo_simulation.py
+++ monte_carlo_simulation.py
@@ -5,6 +5,21 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Parameters
+a =2                            # radius and side length of the square
+circle_center = (4*a, 0)
+
+square_bottom_left = (0, -a/2)
+
+num_point = 3000
+
+# Setup Figures
+fig, ax = plt.subplots(figsize=(10,5))
+ax.set_xlim(-a, 6*a)
+
+
+
+
+
 
 # Setup Figures
 
