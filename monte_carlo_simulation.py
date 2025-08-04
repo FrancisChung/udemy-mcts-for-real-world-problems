b30a10fc7e18cea0eb27f8e856a02eb7e3e328c0
Francis Chung <francis.c.chung@gmail.com>
2025-08-04 13:19:42 +1000

    Draw Square Code

--- monte_carlo_simulation.py
+++ monte_carlo_simulation.py
@@ -15,14 +15,12 @@ num_point = 3000
 # Setup Figures
 fig, ax = plt.subplots(figsize=(10,5))
 ax.set_xlim(-a, 6*a)
-
-
-
-
-
-
-# Setup Figures
+ax.set_ylim(-1.5*a, 1.5*a)
+ax.set_aspect('equal')
+ax.set_title('Monte Carlo Simulation')
 
 # Draw Square
+square = plt. Rectangle(square_bottom_left, a, a, edgecolor = 'green', facecolor = 'none', linewidth = 2, label = 'Square')
+ax.Add_patch(square)
 
 # Draw Circle
